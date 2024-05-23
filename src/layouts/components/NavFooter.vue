cfc0d0ba6de0910dba68302bec4e6e32ccca22a4
validator asia <admin@validatorasia.com>
2024-05-23 22:23:53 +0700

    init explorer

---
+++ src/layouts/components/NavFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- footer -->
+  <footer
+    class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
+  >
+    <div class="flex flex-1">
+      &copy;&nbsp;
+      {{ new Date().getFullYear() }}&nbsp;
+      Made With&nbsp;
+      <img src="../../assets/images/heart.svg" />&nbsp;
+      By&nbsp;
+      <a
+        class="link link-primary no-underline"
+        href="https://validatorasia.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >ValidatorAsia</a
+      >
+    </div>
+    <div
+      class="hidden md:!block"
+    >
+      <a
+        class="link link-primary no-underline"
+        href="https://github.com/john839361"
+        target="noopener noreferrer"
+        >Github</a
+      >
+    </div>
+  </footer>
+</template>
